cf156c48086940cd75cee2329135d4cac826d73d
Alina Malginova <39488063+AlinaMalginova1998@users.noreply.github.com>
2022-07-26 14:06:23 +0300

    Task 97 | Кнопка оглавление, окно оглавления, адаптив, JS (#105)

--- scripts/sp.js
+++ scripts/sp.js
@@ -1,8 +1,18 @@
 const spPopup = document.querySelector('.sp-popup');
 const spPopupButtonClose = spPopup.querySelector('.sp-popup__close');
 
+const spPopupTable = document.querySelector('.table-popup');
+const spPopupTableCloseButton = spPopupTable.querySelector('.table-popup__close-button');
+const spPopupTableOpenButton = document.querySelector('.rotated-button');
+
 function closeSpPopup(modal) {
   modal.classList.add('popup_closed');
 }
 
+function openSpPopup(modal) {
+  modal.classList.remove('popup_closed');
+}
+
 spPopupButtonClose.addEventListener('click', () => closeSpPopup(spPopup));
+spPopupTableCloseButton.addEventListener('click', () => closeSpPopup(spPopupTable));
+spPopupTableOpenButton.addEventListener('click', () => openSpPopup(spPopupTable));
