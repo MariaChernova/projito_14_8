979515c5cf445afdd116152a0d0ab6166f13c36e
Alina Malginova <39488063+AlinaMalginova1998@users.noreply.github.com>
2022-07-26 12:06:50 +0300

    переехала на новую ветку

--- scripts/sp.js
+++ scripts/sp.js
@@ -1,8 +1,12 @@
 const spPopup = document.querySelector('.sp-popup');
 const spPopupButtonClose = spPopup.querySelector('.sp-popup__close');
 
+const spPopupTable = document.querySelector('.table-popup');
+const spPopupTableCloseButton = spPopupTable.querySelector('.table-popup__close-button');
+
 function closeSpPopup(modal) {
   modal.classList.add('popup_closed');
 }
 
 spPopupButtonClose.addEventListener('click', () => closeSpPopup(spPopup));
+spPopupTableCloseButton.addEventListener('click', () => closeSpPopup(spPopupTable));
