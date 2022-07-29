9d9d4b7c6a5d49e293e6f1896c8be8044b786ac2
Ser2204 <104269296+Ser2204@users.noreply.github.com>
2022-07-29 21:40:10 +0300

    Task 127 6. функциональность на js работает некорректно (#137)

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-const greenPopup = document.querySelector('.green-popup');
-const greenPopupButtonClose = greenPopup.querySelector('.green-popup__close');
+const greenPopup = document.querySelector(".green-popup");
+const greenPopupButtonClose = greenPopup.querySelector(".green-popup__close");
 
 const btnScrRigthNews = document.querySelector("#btnScrRigthNews");
 const btnScrLeftNews = document.querySelector("#btnScrLeftNews");
@@ -9,8 +9,13 @@ const btnScrRigthMagaz = document.querySelector("#btnScrRigthMagaz");
 const btnScrLeftMagaz = document.querySelector("#btnScrLeftMagaz");
 const sliderMagaz = document.querySelector("#sliderMagaz");
 
+const diarysContainer = document.querySelector("#diarysContainer");
+const btnMaterSlideEl1 = document.querySelector("#materSlideEl1");
+const btnMaterSlideEl2 = document.querySelector("#materSlideEl2");
+const btnMaterSlideEl3 = document.querySelector("#materSlideEl3");
+
 function closeGreenPopup(modal) {
-  modal.classList.add('popup-closer');
+  modal.classList.add("popup-closer");
 }
 
 function activateBtn(slide) {
@@ -21,8 +26,9 @@ function disactivateBtn(slide) {
   slide.classList.add("slider__slide-button_disabled");
 }
 
-
-greenPopupButtonClose.addEventListener('click', () => closeGreenPopup(greenPopup));
+greenPopupButtonClose.addEventListener("click", () =>
+  closeGreenPopup(greenPopup)
+);
 
 btnScrRigthNews.addEventListener("click", function () {
   sliderNews.scrollBy(348, 0);
@@ -40,4 +46,25 @@ btnScrRigthMagaz.addEventListener("click", function () {
 
 btnScrLeftMagaz.addEventListener("click", function () {
   sliderMagaz.scrollBy(-348, 0);
+});
+
+btnMaterSlideEl1.addEventListener("click", function () {
+  diarysContainer.scrollTo(0, 0);
+  btnMaterSlideEl1.classList.add("slide__ellipse_active");
+  btnMaterSlideEl2.classList.remove("slide__ellipse_active");
+  btnMaterSlideEl3.classList.remove("slide__ellipse_active");
+});
+
+btnMaterSlideEl2.addEventListener("click", function () {
+  diarysContainer.scrollTo(276, 0);
+  btnMaterSlideEl2.classList.add("slide__ellipse_active");
+  btnMaterSlideEl1.classList.remove("slide__ellipse_active");
+  btnMaterSlideEl3.classList.remove("slide__ellipse_active");
+});
+
+btnMaterSlideEl3.addEventListener("click", function () {
+  diarysContainer.scrollTo(552, 0);
+  btnMaterSlideEl3.classList.add("slide__ellipse_active");
+  btnMaterSlideEl2.classList.remove("slide__ellipse_active");
+  btnMaterSlideEl1.classList.remove("slide__ellipse_active");
 });
